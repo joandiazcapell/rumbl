b2b95333535b80bbf00cd59b452f4a81baa0a2a2
Joan Diaz <jdiaz@newrelic.com>
2024-03-03 18:51:28 +0100

    Add new users to the DB and manage errors

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -18,4 +18,16 @@ defmodule RumblWeb.UserController do
     changeset = Accounts.change_user(%User{})
     render(conn, :new, changeset: changeset)
   end
+
+  def create(conn, %{"user" => user_params}) do
+    case Accounts.create_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} creatred!")
+        |> redirect(to: ~p"/users")
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :new, changeset: changeset)
+    end
+
+  end
 end
