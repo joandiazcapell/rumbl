a3611dab79f8cf418cdc57897867bd4c4134a6e5
Joan Diaz <jdiaz@newrelic.com>
2024-03-03 18:24:16 +0100

    add a new user form

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -8,4 +8,11 @@ defmodule Rumbl.Accounts.User do
 
     timestamps(type: :utc_datetime)
   end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :username])
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
+  end
 end
