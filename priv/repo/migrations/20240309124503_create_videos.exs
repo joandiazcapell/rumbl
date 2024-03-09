ed6532233018c02fe7b074a1de14e367e16f59a1
Joan Diaz <jdiaz@newrelic.com>
2024-03-09 16:23:59 +0100

    fix(video): user_id changed instead of use_id

--- priv/repo/migrations/20240309124503_create_videos.exs
+++ priv/repo/migrations/20240309124503_create_videos.exs
@@ -6,11 +6,11 @@ defmodule Rumbl.Repo.Migrations.CreateVideos do
       add :url, :string
       add :title, :string
       add :description, :text
-      add :use_id, references(:users, on_delete: :nothing)
+      add :user_id, references(:users, on_delete: :nothing)
 
       timestamps(type: :utc_datetime)
     end
 
-    create index(:videos, [:use_id])
+    create index(:videos, [:user_id])
   end
 end
