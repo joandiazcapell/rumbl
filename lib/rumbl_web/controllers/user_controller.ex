c0223816f82913d822a349a4a160df73f2672e50
Joan Diaz <jdiaz@newrelic.com>
2024-03-09 10:07:21 +0100

    New users with password

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -14,13 +14,13 @@ defmodule RumblWeb.UserController do
     render(conn, :show, user: user)
   end
 
-  def new(conn, _params) do
-    changeset = Accounts.change_user(%User{})
+  def new(conn, params) do
+    changeset = Accounts.change_registration(%User{}, params)
     render(conn, :new, changeset: changeset)
   end
 
   def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
+    case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
         |> put_flash(:info, "#{user.name} creatred!")
