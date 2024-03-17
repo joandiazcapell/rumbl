dde169aec7b665b0e5d455bf288cc7c1523926c3
Joan Diaz <jdiaz@newrelic.com>
2024-03-17 21:08:57 +0100

    test for multimedia context

--- test/rumbl/accounts_test.exs
+++ test/rumbl/accounts_test.exs
@@ -12,7 +12,6 @@ defmodule Rumbl.AccountsTest do
       password: "secret"
     }
     @invalid_attrs %{}
-  end
 
   test "with valid data inserts user" do
     assert {:ok, %User{id: id} = user} = Accounts.register_user(@valid_attrs)
@@ -50,5 +49,27 @@ defmodule Rumbl.AccountsTest do
     assert %{password: ["should be at least 6 character(s)"]} = errors_on(changeset)
     assert Accounts.list_users() == []
   end
+end
 
+  describe "authenticate_by_username_and_password/2" do
+    @pass "123456"
+
+    setup do
+      {:ok, user: user_fixture(password: @pass)}
+    end
+
+    test "returns user with correct password", %{user: user} do
+      assert {:ok, auth_user} = Accounts.authenticate_by_username_and_password(user.username, @pass)
+
+      assert auth_user.id == user.id
+    end
+
+    test "returns unauthorized error with invalid password", %{user: user} do
+      assert {:error, :unauthorized} = Accounts.authenticate_by_username_and_password(user.username, "badpass")
+    end
+
+    test "returns not found error with no matching user for email" do
+      assert {:error, :not_found} = Accounts.authenticate_by_username_and_password("unknownuser", @pass)
+    end
+  end
 end
