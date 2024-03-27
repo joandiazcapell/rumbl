af7e83a7acf02fb373ff8719b5370b9c132a759e
Joan Diaz <jdiaz@newrelic.com>
2024-03-27 17:59:26 +0100

    Sockets with authentication

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -2,25 +2,11 @@ defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
 
   @impl true
-  def join("videos:" <> video_id, _payload, socket) do
-    :timer.send_interval(5_000, :ping)
-    {:ok, socket}
+  def join("videos:" <> video_id, payload, socket) do
+    #:timer.send_interval(5_000, :ping)
     #{:ok, assign(socket, :video_id, String.to_integer(video_id))}
-    #if authorized?(payload) do
-    #  {:ok, socket}
-    #else
-    #  {:error, %{reason: "unauthorized"}}
-    #end
+    {:ok, socket}
   end
-
-  #@impl true
-  #def handle_info(:ping, socket) do
-  #  count = socket.assigns[:count] || 1
-  #  push(socket, "ping", %{count: count})
-
-  #  {:noreply, assign(socket, :count, count + 1)}
-  #end
-
 
   @impl true
   def handle_in("new_annotation", payload, socket) do
@@ -31,10 +17,5 @@ defmodule RumblWeb.VideoChannel do
     })
 
     {:reply, :ok, socket}
-  end
-
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
   end
 end
--- lib/rumbl_web/controllers/plugs/auth.ex
+++ lib/rumbl_web/controllers/plugs/auth.ex
@@ -9,15 +9,15 @@ defmodule RumblWeb.Plugs.Auth do
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
     cond do
-      conn.assigns[:current_user] -> conn
-      user = user_id && Rumbl.Accounts.get_user(user_id) ->  assign(conn, :current_user, user)
+      user = conn.assigns[:current_user] -> put_current_user(conn, user)
+      user = user_id && Rumbl.Accounts.get_user(user_id) ->  put_current_user(conn, user)
       true -> assign(conn, :current_user, nil)
     end
   end
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -35,5 +35,13 @@ defmodule RumblWeb.Plugs.Auth do
       |> redirect(to: ~p"/")
       |> halt()
     end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
  end
